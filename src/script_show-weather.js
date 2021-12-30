a2a205a6a52d68ea3d494f65280619b6a3580007
crispolilla <c.arduz.c@gmail.com>
2021-12-29 20:08:21 -0400

    API integration today-weather

---
+++ src/script_show-weather.js
@@ -0,0 +1,21 @@
+function showTemp (response) {
+  let tempMax = Math.round(response.data.main.temp_max);
+  let tempMin = Math.round(response.data.main.temp_min);
+  let tempMaxEl = document.querySelector("#temp-max");
+  let tempMinEl = document.querySelector("#temp-min");
+  let descriptionEl = document.querySelector("#description");
+  let rainfallEl = document.querySelector("#rain");
+  tempMaxEl.innerHTML = `${tempMax}° C`;
+  tempMinEl.innerHTML = `${tempMin}°`;
+  descriptionEl.innerHTML = `${response.data.weather[0].description}`;
+  rainfallEl.innerHTML = ` ${response.data.main.humidity}%`;
+  let cityEl = document.querySelector("#city");
+  cityEl.innerHTML = `${response.data.name}`;
+}
+
+let city = `Chicago`;
+
+let apiKey = `87bb877dc5b8cdcd202ebaa9f56f9365`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemp);
