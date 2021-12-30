bc4f2199ca64970f0095f938e03eb0797d078b17
crispolilla <c.arduz.c@gmail.com>
2021-12-29 21:03:42 -0400

    functioning search for my location

--- src/script_index.js
+++ src/script_index.js
@@ -8,5 +8,26 @@ function goToWeather (event) {
   else {alert("Please enter a city");}
 }
 
+function logMyLocation (response) {
+  let cityInput = response.data.name;
+  window.location.href = `show-weather.html?city=${cityInput}`;
+}
+
+function findMyLocation (position) {
+  let lon = position.coords.longitude;
+  let lat = position.coords.latitude;
+  let apiKey = `87bb877dc5b8cdcd202ebaa9f56f9365`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(logMyLocation);
+}
+
+function fetchMyLocation (event) {
+  navigator.geolocation.getCurrentPosition(findMyLocation);
+}
+
 let citySearch = document.querySelector("#city-search");
 citySearch-addEventListener("submit",goToWeather);
+
+let myLocation = document.querySelector("#my-location");
+myLocation.addEventListener("click", fetchMyLocation);
+
